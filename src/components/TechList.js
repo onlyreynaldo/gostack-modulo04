6968f944a7d76c74aa29265c093a37399f64fed2
Reynaldo Layme <layme.reynaldo@icloud.com>
2020-02-08 22:15:37 -0300

    Adding states concepts

--- src/components/TechList.js
+++ src/components/TechList.js
@@ -1,13 +1,42 @@
 import React, { Component } from 'react';
 
 class TechList extends Component {
+  state = {
+    newTech: '',
+    techs: [
+      'Node.js',
+      'ReactJS',
+      'React Native'
+    ]
+  };
+
+  handleImputChange = e => {
+    this.setState({ newTech: e.target.value });
+  }
+
+  handleSubmit = e => {
+    e.preventDefault();
+
+    this.setState({
+      techs: [ ...this.state.techs, this.state.newTech ],
+      newTech: ''
+    });
+  }
+
   render() {
     return (
-      <ul>
-        <li>Node.js</li>
-        <li>ReactJS</li>
-        <li>React Native</li>
-      </ul>
+      <form onSubmit={this.handleSubmit}>
+        <ul>
+          { this.state.techs.map( tech => <li key={tech}>{tech}</li> ) }
+        </ul>
+        <h1>{ this.state.newTech }</h1>
+        <input 
+          type="text"
+          onChange={this.handleImputChange}
+          value={this.state.newTech}
+        />
+        <button type="submit">Enviar</button>
+      </form>
     );
   }
 }
